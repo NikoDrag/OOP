2d0cfa2402ac73874886d61174a6d23d4f9ee056
NikoDrag <126951268+NikoDrag@users.noreply.github.com>
2023-05-04 23:00:08 +0200

    Add files via upload

---
+++ Kontakti/Kontakti/Adresar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontakti
+{
+    class Adresar
+    {
+        public Dictionary<string, Kontakt> Kontakti = new Dictionary<string, Kontakt>();
+
+        public void Dodaj(Kontakt k)
+        {
+            Kontakti.Add(k.GlavniBroj, k);
+        }
+        public void Dodaj(string ime, string prezime, string glavnibroj)
+        {
+            Kontakti.Add(glavnibroj, new Kontakt(ime, prezime, glavnibroj));
+        }
+        public void Blokiraj(string broj)
+        {
+            Kontakti.Remove(broj);
+        }
+
+        public void Share(string file, string glavniBroj)
+        {
+            
+        }
+        //kreira fajl sa informacijama o kontaktu sa zadatim brojem
+        
+        public void Receive(string file)
+        {
+
+        } 
+        //čita informacije iz binarnog fajla koji sadrži informacije o jednom
+        //kontaktu i dodaje novi kontakt u kolekciju ukoliko kontakt sa tim glavnim brojem već ne postoji.
+        
+        public void Backup(string file)
+        {
+
+        }
+        //čuva sve informacije o kontaktima u zadatom tekstualnom fajlu na
+        //sledeći način:
+
+
+    }
+}
---
+++ Kontakti/Kontakti/Kontakt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontakti
+{
+    class Kontakt
+    {
+        private string ime;
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        private string prezime;
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        private string glavnibroj;
+
+        public string GlavniBroj
+        {
+            get { return glavnibroj; }
+            set { glavnibroj = value; }
+        }
+
+        private List<string> ostalibrojevi = new List<string>();
+
+        public Kontakt(string ime, string prezime, string glavnibroj)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.glavnibroj = glavnibroj;
+        }
+
+
+        public void DodajBroj(string dodatnibroj)
+        {
+            foreach(string broj in ostalibrojevi)
+            {
+                if (broj == dodatnibroj)
+                    return;
+            }
+            ostalibrojevi.Add(dodatnibroj);
+        }
+
+
+        public void IzbrisiBroj(string dodatniBroj)
+        {
+            ostalibrojevi.Remove(dodatniBroj);
+        }
+
+        public override string ToString()
+        {
+            return $"{ime}, {Prezime}, {glavnibroj}, [{ostalibrojevi}]";
+        }
+    }
+}
